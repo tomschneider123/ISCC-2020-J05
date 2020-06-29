4eb78eb3dba4b7f285a28e9adae0a6fc101673a5
Tom Schneider <tom.schneider@iseg.fr>
2020-06-29 15:39:38 +0200

    Create t-shirt.php

---
+++ EX_03/t-shirt.php
@@ -0,0 +1,59 @@
+<?php
+$nom_produit = "T-shirt simple";
+$couleur = "Blanc";
+$prix = 10,50;
+$disponible = true;
+$quantite = 10;
+$a = $prix * 3;
+$b = $prix * $quantite;
+$c = 3 - $quantite;
+<h3>
+echo "Le nom du produit est ";
+echo $nom_produit;
+echo ".";
+</h3>
+<h3>
+echo "Il reste ";
+echo $quantite;
+echo " produit en stock.";
+</h3>
+<h3>
+echo "Le produit ";
+echo $nom_produit;
+echo "est de couleur ";
+echo $couleur;
+echo ".";
+</h3>
+
+<h4>
+echo "Acheter 3 produits couterait "
+echo $a;
+echo "."
+</h4>
+<h4>
+echo "Acheter la totalité des produits disponibles coûterait "
+echo $b;
+echo "."
+</h4>
+<h4>
+echo "Si 3 produits sont vendus, alors il reste "
+echo $c;
+echo " produits en stock."
+</h4>
+if ($quantite > 0)
+{
+    echo "Le produit ";
+    echo $nom_produit;
+    echo " est disponible en ligne.";
+    $disponible = "true";
+}
+elseif ($quantite < 0)
+{
+    echo "Le produit";
+    echo $nom_produit;
+    echo " n'est malheureusement plus disponible.";
+    $disponible = "false";
+
+}
+
+?>
