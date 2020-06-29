d4a7c9268569fb73a81270c8b7c6005dcb9b8c2c
Tom Schneider <tom.schneider@iseg.fr>
2020-06-29 16:37:40 +0200

    Update t-shirt.php

--- EX_03/t-shirt.php
+++ EX_03/t-shirt.php
@@ -1,45 +1,59 @@
 <?php
 $nom_produit = "T-shirt simple";
 $couleur = "Blanc";
-$prix = 10,50;
+$prix = 10.50;
 $disponible = true;
 $quantite = 10;
 $a = $prix * 3;
 $b = $prix * $quantite;
 $c = 3 - $quantite;
+?>
 <h3>
+<?php
 echo "Le nom du produit est ";
 echo $nom_produit;
 echo ".";
+?>
 </h3>
 <h3>
+<?php
 echo "Il reste ";
 echo $quantite;
 echo " produit en stock.";
+?>
 </h3>
 <h3>
+<?php
 echo "Le produit ";
 echo $nom_produit;
 echo "est de couleur ";
 echo $couleur;
 echo ".";
+?>
 </h3>
 
 <h4>
-echo "Acheter 3 produits couterait "
-echo $a;
-echo "."
+<?php
+echo "Acheter 3 produits couterait ";
+echo "$a";
+echo ".";
+?>
 </h4>
 <h4>
-echo "Acheter la totalité des produits disponibles coûterait "
+<?php
+echo "Acheter la totalité des produits disponibles coûterait ";
 echo $b;
-echo "."
+echo ".";
+?>
 </h4>
 <h4>
-echo "Si 3 produits sont vendus, alors il reste "
+<?php
+echo "Si 3 produits sont vendus, alors il reste ";
 echo $c;
-echo " produits en stock."
+echo " produits en stock.";
+?>
 </h4>
+<?php
 if ($quantite > 0)
 {
     echo "Le produit ";
@@ -53,7 +67,6 @@ elseif ($quantite < 0)
     echo $nom_produit;
     echo " n'est malheureusement plus disponible.";
     $disponible = "false";
-
+?>
 }
 
-?>
